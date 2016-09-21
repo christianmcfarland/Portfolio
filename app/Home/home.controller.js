dcc445c5fe9b65cdbbdc09e24090eeb2f759639f
c-mac88 <christianmcfarland@gmail.com>
2016-09-20 20:51:57 -0700

    added slider to change colors

--- app/Home/home.controller.js
+++ app/Home/home.controller.js
@@ -21,7 +21,19 @@
 
         vm.trueistrue = false;
 
+        var slider = new Slider('#ex1', {
+            formatter: function(value) {
+                return 'Current value: ' + value;
+            }
+        });
 
+        vm.colorChanger = function() {
+            if (slider.getValue() < 10) {
+                console.log("black");
+            } else {
+                console.log("blue");
+            }
+        }
 
     }
 })();
