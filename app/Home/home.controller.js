7f453b8889246d94caf9a6d1119aa80106d467df
c-mac88 <christianmcfarland@gmail.com>
2016-10-09 14:02:15 -0700

    Refactored website to be only my AngularJS portfolio

--- app/Home/home.controller.js
+++ app/Home/home.controller.js
@@ -13,27 +13,8 @@
         //using vm
         var vm = this;
 
-        vm.educations = ["Origin Code Academy", "Palm Beach State College"]
+        vm.calcShowing = false;
 
-        vm.experiences = ["Matrix Mediation", "Adventure Pass"]
-
-        vm.examples = ["Adventure Pass", "AngularJS Projects", "Chirper", "Loop Scheduler", "Super Joe Campers"]
-
-        vm.trueistrue = false;
-
-        var slider = new Slider('#ex1', {
-            formatter: function(value) {
-                return 'Current value: ' + value;
-            }
-        });
-
-        vm.colorChanger = function() {
-            if (slider.getValue() < 10) {
-                console.log("black");
-            } else {
-                console.log("blue");
-            }
-        }
 
     }
 })();
